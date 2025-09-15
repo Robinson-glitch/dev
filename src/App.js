b10a3cbf7e3647f0ac8a9b320e053e44dc33bd63
mariarobin895@gmail.com <mariarobin895@gmail.com>
2025-09-15 16:44:32 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+
+
+// import logo from './logo.svg';
+import React, { useState,useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+const[countries,Setcountries]=useState([]);
+const[states, Setstates]=useState([]);
+const[cities, Setcities]=useState([]);
+const[selectedCountry,setSelectedCountry]=useState("");
+const[selectedState, setSelectedState]=useState("");
+const[selectedCity,setSelectedCity]=useState("");
+
+useEffect(()=>{
+  
+fetchCountries();
+
+},[])
+
+ const fetchCountries=()=>{
+
+  axios.get("https://crio-location-selector.onrender.com/countries").then((response)=>{
+
+    Setcountries(response.data);
+    console.log(response.data);
+
+  }).catch((error)=>{
+
+    console.log(`Error fetching data:${error}`);
+  });
+  }
+  
+  const handleSelectCountry=(event)=>{
+    
+    setSelectedCountry(event.target.value)
+    fetchStates(event.target.value);
+  }
+
+   const handleSelectState=(event)=>{
+    const SelectedState=event.target.value;
+    setSelectedState(event.target.value)
+    fetchCities(selectedCountry,SelectedState);
+  }
+
+   const handleSelectCity=(event)=>{
+    setSelectedCity(event.target.value)
+
+  }
+
+  const fetchStates=(countryName)=>{
+  axios.get(`https://crio-location-selector.onrender.com/country=${countryName}/states`).then((response)=>{
+
+    Setstates(response.data);
+
+  }).catch((error)=>{
+
+    console.log(`Error fetching data:${error}`);
+  });
+    
+  }
+
+  const fetchCities=(countryName,stateName)=>{
+  axios.get(`https://crio-location-selector.onrender.com/country=${countryName}/state=${stateName}/cities`).then((response)=>{
+
+    Setcities(response.data);
+
+  }).catch((error)=>{
+
+    console.log(`Error fetching data:${error}`);
+  });
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:"flex",gap:"10px"}}>
+    <select  value={selectedCountry} onChange={handleSelectCountry}>
+    <option value="">Select Country</option>
+     {countries.map((country)=>(
+     <option key={country} value={country}>{country}</option>
+     ))}
+    </select>
+    <select disabled={selectedCountry!==""?false:true} value={selectedState} onChange={handleSelectState}>
+    <option value="">Select State</option>
+    {states.map((state)=>(
+      <option key={state} value={state}>{state}</option>
+    ))}
+    </select>
+    <select  disabled={selectedState!==""?false:true} value={selectedCity} onChange={handleSelectCity}>
+    <option value="">Select City</option>
+    {cities.map((city)=>(
+<option key={city} value={city}>{city}</option>
+    ))}
+    </select>
     </div>
   );
 }
 
 export default App;
+
+
+
+
